457c52c3ab7559d15afda309687699108f044bc3
Felipe Moreira <ti.felipemoreira@gmail.com>
2022-02-24 20:12:46 -0300

    commit inicial

---
+++ src/components/layout/navbar.tsx
@@ -0,0 +1,69 @@
+import { Menu } from "../icons"
+import { useState } from 'react';
+export default function NavBar() {
+    // Declarar uma nova variável de state, na qual chamaremos de "count"
+    const [open, setOpen] = useState(false);
+    const [openDropdown, setOpenDropdown] = useState(false);
+    const a = "flex px-4 py-2 font-medium rounded-md text-white hover:bg-blue-800  "
+    const ul = "p-4"
+    const dropdownLi = "flex p-2 font-medium text-gray-600 rounded-md hover:bg-gray-100 hover:text-black "
+    const style = { a, ul,dropdownLi }
+    const openMenu = true
+    return (
+        <nav className="bg-blue-700">
+            {/* Container */}
+            <div className="container flex flex-wrap mx-auto py-2 ">
+                {/* Brand */}
+                <a className="inline-flex p-2 text-xl font-bold tracking-wider text-white"
+                    href="#">BeF <span className="text-black"> Consultoria</span>
+                </a>
+                {/* Brand */}
+                {/* Toggler btn */}
+                <button onClick={() => setOpen(!open)} className="lg:hidden inline-flex items-center justify-center ml-auto border h-10 w-10 rounded-md outline-none focus:outline-nome"
+                >
+                    {Menu}
+                </button>
+                {/* Toggler btn */}
+                {/* Menu */}
+                <div className={open ? "w-full p-2 lg:inline-flex lg:w-auto mt-2 lg:mt-0" : "hidden lg:inline-flex lg:w-auto lg:mt-0"} >
+                    <ul className="flex flex-col lg:flex-row space-y-2 lg:space-y-0 lg:space-x-4">
+                        <li >
+                            <a className={`${style.a} bg-blue-900`} href="#">Dashbord</a>
+                        </li>
+                        <li >
+                            <a className={style.a} href="#">Projects</a>
+                        </li>
+                        {/* Dropdown */}
+                        <li className="relative">
+                            <button onMouseEnter={() => setOpenDropdown(!openDropdown)}   className={`${style.a} outline-none focus:outline-none`}>Dropdown </button>
+                        {/* Dropdown Menu */}
+                        <div className={openDropdown ? "lg:absolute bg-white right-0 rouded-md p-2 top-12":"hidden" }>
+                            <ul onMouseLeave={() => setOpenDropdown(false)} className="space-y-2 lg:w-24">
+                                <li >
+                                    <a  className={`${style.dropdownLi}`} href="#">Categorias</a>
+                                </li>
+                                <li>
+                                    <a className={`${style.dropdownLi}`} href="#">Inventories</a>
+                                </li>
+                                <li>
+                                    <a className={`${style.dropdownLi}`} href="#">Categorias</a>
+                                </li>
+                            </ul>
+                        </div>
+                        {/* Dropdown Menu */}
+                        </li>
+                        {/* Dropdown */}
+                        <li  >
+                            <a className={style.a} href="#">Orders</a>
+                        </li>
+                        <li >
+                            <a className={style.a} href="#">Invoices</a>
+                        </li>
+                    </ul>
+                </div>
+                {/* Menu */}
+            </div>
+
+        </nav>
+    )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import NavBar from '../components/layout/navbar'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <NavBar></NavBar>
+    </div>
+  )
+}
+
+export default Home
